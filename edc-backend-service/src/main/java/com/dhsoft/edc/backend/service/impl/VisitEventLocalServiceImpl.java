d485492664a62740a7af81688af72b0d726b5f5c
DHS-Lee <sjlee@dhsoft.co.kr>
2025-11-19 14:01:05 +0900

    visitEventLocalServiceImpl에 updateEventDate, updateCRFData 메서드 추가

--- edc-backend-service/src/main/java/com/dhsoft/edc/backend/service/impl/VisitEventLocalServiceImpl.java
+++ edc-backend-service/src/main/java/com/dhsoft/edc/backend/service/impl/VisitEventLocalServiceImpl.java
@@ -14,9 +14,14 @@
 
 package com.dhsoft.edc.backend.service.impl;
 
+import com.dhsoft.edc.backend.model.VisitEvent;
 import com.dhsoft.edc.backend.service.base.VisitEventLocalServiceBaseImpl;
-
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -28,4 +33,55 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class VisitEventLocalServiceImpl extends VisitEventLocalServiceBaseImpl {
+	
+	//When subject visit, update VisitEvent
+	public void updateEventDate(long visitEventId, Date eventDate, String deviationStatus) {
+		try {
+			Date modifiedDate = new Date();
+			VisitEvent v = visitEventPersistence.findByPrimaryKey(visitEventId);
+			v.setEventDate(eventDate);
+			v.setDeviationStatus(deviationStatus);
+			v.setModifiedDate(modifiedDate);
+			visitEventPersistence.update(v);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	//When CRF Save, update VisitEvent
+	public void updateCRFData(long visitEventId, long structuredDataId) {
+		try {
+			VisitEvent v = visitEventPersistence.findByPrimaryKey(visitEventId);
+			Date modifiedDate = new Date();
+			String linkObj = v.getInstanceLinkObj();
+			JSONObject rootJson;
+	        JSONArray linksArray;
+			if(linkObj == null || linkObj.trim().isEmpty()) {
+				
+				rootJson = JSONFactoryUtil.createJSONObject();
+	            linksArray = JSONFactoryUtil.createJSONArray();
+	            
+	            linksArray.put(structuredDataId);
+	            rootJson.put("links", linksArray);
+	            
+			} else {
+				
+				rootJson = JSONFactoryUtil.createJSONObject(linkObj);
+				linksArray = rootJson.getJSONArray("links");
+				if (linksArray == null) {
+	                linksArray = JSONFactoryUtil.createJSONArray();
+	            }
+				linksArray.put(structuredDataId);
+				rootJson.put("links", linksArray);
+
+			}
+			v.setModifiedDate(modifiedDate);
+			visitEventPersistence.update(v);
+			
+		} catch (Exception e) {
+			
+		}
+	}
 }
